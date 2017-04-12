885b014406e5effcd0c218ec32604e4002a04147
silvio <silvio.barbotto@gmail.com>
2017-04-12 21:48:31 +0200

    student_2 non tested

--- implementazione di un DFA in Java/esercizio 1.4/Student_2.java
+++ implementazione di un DFA in Java/esercizio 1.4/Student_2.java
@@ -70,3 +70,179 @@
       f( 7, [0-9] ) = 8
 
       f( 8, " "+[A-Z]+[0-9]+[a-z] ) = 8
+*/
+
+      public class Student{
+
+         public static boolean scan( String s ){
+
+            int state = 0;
+            int i = 0;
+
+            while( state >= 0 && i < s.length() ){
+
+               final char ch = s.charAt( i++ );
+
+               switch( state ){
+
+                  case 0:
+
+                     if( ch == ' ') state = 0;
+
+                     else if( ch == '0' ||
+                              ch == '2' ||
+                              ch == '4' ||
+                              ch == '6' ||
+                              ch == '8'   ) state = 1;
+
+                     else if( ch == '1' ||
+                              ch == '3' ||
+                              ch == '5' ||
+                              ch == '7' ||
+                              ch == '9'   ) state = 2;
+
+                     else if( ch >= 'A' && ch <= 'z' ) state = 8;
+
+                     else state = -1;
+
+                        break;
+
+                  case 1:
+
+                     if( ch == ' ' ) state = 3;
+
+                     else if( ch == '0' ||
+                              ch == '2' ||
+                              ch == '4' ||
+                              ch == '6' ||
+                              ch == '8'   ) state = 1;
+
+                     else if( ch == '1' ||
+                              ch == '3' ||
+                              ch == '5' ||
+                              ch == '7' ||
+                              ch == '9'   ) state = 2;
+
+                     else if( ch >= 'A' && ch <= 'K' ) state = 5;
+
+                     else if( ch >= 'L' && ch <= 'Z' ||
+                              ch >= 'a' && ch <= 'z' ||
+                              ch >= '0' && ch <= '9'   ) state = 8;
+
+                     else state = -1;
+
+                        break;
+
+                  case 2:
+
+                     if( ch == ' ' ) state = 4;
+
+                     else if( ch == '0' ||
+                              ch == '2' ||
+                              ch == '4' ||
+                              ch == '6' ||
+                              ch == '8'   ) state = 1;
+
+                     else if( ch == '1' ||
+                              ch == '3' ||
+                              ch == '5' ||
+                              ch == '7' ||
+                              ch == '9'   ) state = 2;
+
+                     else if( ch >= 'L' && ch <= 'Z' ) state = 5;
+
+                     else if( ch >= 'A' && ch <= 'K' ||
+                              ch >= 'a' && ch <= 'z'   ) state = 8;
+
+                     else state = -1;
+
+                        break;
+
+                  case 3:
+
+                     if( ch == ' ' ) state = 3;
+
+                     else if( ch >= 'A' && ch <= 'K' ) state = 5;
+
+                     else if( ch >= 'L' && ch <= 'Z' ||
+                              ch >= 'a' && ch <= 'z' ||
+                              ch >= '0' && ch <= '9'   ) state = 8;
+
+                     else state = -1;
+
+                        break;
+
+                  case 4:
+
+                     if( ch == ' ' ) state = 4;
+
+                     else if( ch >= 'L' && ch <= 'Z' ) state = 5;
+
+                     else if( ch >= 'A' && ch <= 'K' ||
+                              ch >= 'a' && ch <= 'z' ||
+                              ch >= '0' && ch <= '9'   ) state = 8;
+
+                     else state = -1;
+
+                        break;
+
+                  case 5:
+
+                     if( ch == ' ' ) state = 6;
+
+                     else if( ch >= 'a' && ch <= 'z' ) state = 7;
+
+                     else if( ch >= 'A' && ch <= 'Z' ||
+                              ch >= '0' && ch <= '9'   ) state = 8;
+
+                     else state = -1;
+
+                        break;
+
+                  case 6:
+
+                     if( ch == ' ' ) state = 6;
+
+                     else if( ch >= 'A' && ch <= 'Z' ) state = 5;
+
+                     else if( ch >= 'a' && ch <= 'z' ||
+                              ch >= '0' && ch <= '9'   ) state = 8;
+
+                     else state = -1;
+                        break;
+
+                  case 7:
+
+                     if( ch == ' ' ) state = 6;
+
+                     else if( ch >= 'a' && ch <= 'z' ) state = 7;
+
+                     else if( ch >= 'A' && ch <= 'Z' ) state = 5;
+
+                     else if( ch >= '0' && ch <= '9' ) state = 8;
+
+                     else state = -1;
+
+                        break;
+
+                  case 8:
+
+                     if( ch == ' ' ||
+                         ch >= 'a' && ch <= 'z' ||
+                         ch >= 'A' && ch <= 'Z' ||
+                         ch >= '0' && ch <= '9'   ) state = 8;
+
+                     else state = -1;
+
+                        break;
+                        
+               }
+            }
+               return state == 3;
+         }
+
+         public static void main( String[] args ){
+
+            System.out.println( scan( args[ 0 ] ) ? "OK" : "NOPE" );
+         }
+      }
