08913aa95e59f3d316c35bab2797ab6ac87897f1
silvio <silvio.barbotto@gmail.com>
2017-04-13 20:31:09 +0200

    creata classe Tag

---
+++ Analisi lessicale/ esercizio 2.1/Tag.java
@@ -0,0 +1,16 @@
+public class Tag {
+
+   public final static int
+      EOF = -1,
+      NUM = 256,
+      ID = 257,
+      RELOP = 258,
+      IF = 259,
+      ELSE = 260,
+      WHILE = 261,
+      ASSIGN = 262,
+      PRINT = 263,
+      READ = 264,
+      OR = 265,
+      AND = 266;
+}
